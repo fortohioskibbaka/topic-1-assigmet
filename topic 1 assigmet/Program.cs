c60c2be052b19d96890345056f5dfbc3eb80eaaf
Parker Hammond <339720419@GoTVDSB.ca>
2024-09-06 09:17:46 -0400

    finished

--- topic 1 assigmet/Program.cs
+++ topic 1 assigmet/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("hello teacher");
+Console.WriteLine("its Parker");
+Console.WriteLine();
+//coments are usefull because you can send a message or explain what the line of code does 
+//THIS IS MY INTIALS
+Console.WriteLine(" PPPP       H       H");    
+Console.WriteLine(" p    P     H       H");
+Console.WriteLine(" p    P     H       H");
+Console.WriteLine(" PPPPP      HHHHHHHHH");
+Console.WriteLine(" P          H       H");
+Console.WriteLine(" P          H       H");
+Console.WriteLine(" P          H       H");
+Console.ReadLine();
